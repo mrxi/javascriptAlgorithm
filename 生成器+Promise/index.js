704339cdcfd25e5fa5d118e3a8f899facfe9023a
FujiSan <15546293712@163.com>
2023-08-08 17:01:27 +0800

    生成器+promise

---
+++ 生成器+Promise/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2023-08-08 16:45:38
+ * @LastEditTime: 2023-08-08 16:57:22
+ * @LastEditors: FujiSan
+ * @Description: In User Settings Edit
+ * @FilePath: \javascriptAlgorithm\生成器+Promise\index.js
+ */
+  function fn(str){
+    return new Promise((res)=>{
+     setTimeout(()=>{
+      res(str)
+     },2000)
+    })
+  }
+  
+  fn('why').then(res=>{
+     fn(res+'aaa').then(res=>{
+      fn(res+'bbb').then(res=>{
+        console.log(res)
+      })
+     })
+  })
