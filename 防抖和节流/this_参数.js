7cf210d84f2c8296cac8db83e975967ead3422c8
FujiSan <15546293712@163.com>
2023-09-03 21:03:49 +0800

    防抖功能实现

---
+++ 防抖和节流/this_参数.js
@@ -0,0 +1,18 @@
+// 防抖就是 在固定时间内(2秒内) 如果一直触发就不会触发，只有等停止2秒后才会触发
+// 某个事件，若果在此期间有停顿(达不到2秒又触发)，则下次又会重新计时，只有等到2秒后
+// 才会触发一次事件
+// 节流 就是 在 某一段时间内(2秒)后 就会执行一次事件，
+
+
+//防抖实现
+function shake(fn, time) {
+    let Timer = null
+    const _shake = function (...agr) {
+        if (Timer) clearTimeout(Timer)
+        Timer = setTimeout(() => {
+            fn.apply(this,agr)
+        }, time)
+
+    }
+    return _shake
+}
